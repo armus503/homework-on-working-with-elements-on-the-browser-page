1111a736a67e9deb8f27745ef6e919fc8193656d
Ivan E <mr.armus@mail.ru>
2021-09-02 16:29:28 +0400

    commit 02.09.2021

---
+++ .history/js/script_20210830123236.js
@@ -0,0 +1,36 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    },
+    imgs = document.querySelector('.promo__adv'), //.getElementsByTagName('img');
+    promoGenre = document.querySelector('.promo__genre'),
+    promoBg = document.querySelector('.promo__bg'),
+    promoInteractiveList = document.querySelectorAll('.promo__interactive-item');
+
+imgs.remove();
+promoGenre.textContent = 'ДРАМА';
+promoBg.style.backgroundImage = "url(../img/bg.jpg)";
+promoInteractiveList.forEach(item => {
+    item.textContent.length = movieDB;
+});
---
+++ .history/js/script_20210830143820.js
@@ -0,0 +1,43 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const avdImg = document.querySelectorAll('.promo__adv img'), //вызов списка с рекламой
+    promoBg = document.querySelector('.promo__bg'), //получаем элемент для блока с фоном
+    promoGenre = promoBg.querySelector('.promo__genre'), //извлекаем элемент из блока с фоном
+    promoInteractiveList = document.querySelectorAll('.promo__interactive-item');
+
+avdImg.forEach(item => { //перебор элементов списка и удаление по одному
+    item.remove(); //вместо стрелочной Ф можно использовать безимянную
+});
+promoGenre.textContent = 'ДРАМА'; //заменяем текст в документе
+promoBg.style.backgroundImage = 'url("/img/bg.jpg")'; //заменяем фоновую картинку
+
+/*for (let i = 0; i < promoInteractiveList.length; i++) {
+    promoInteractiveList[i].textContent = movieDB;
+}*/
+promoInteractiveList.forEach(item => {
+    item.textContent = movieDB;
+});
